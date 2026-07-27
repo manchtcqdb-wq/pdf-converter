4a9d6e93942c1b121cf5e08f225f524ee3135634
manchtcqdb-wq <manchtcqdb@gmail.com>
2026-07-28 00:49:08 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://pdf-converter-1-kk6k.onrender.com/api'
+const API_BASE_URL = 'https://pdf-converter-2-b98j.onrender.com/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
